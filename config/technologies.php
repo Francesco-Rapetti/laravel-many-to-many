ae889024e25a6cd4bbb76cb698c3f41cf09c2955
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-07 17:15:14 +0100

    Import technology data

---
+++ config/technologies.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        'name' => 'C',
+        'description' => 'The C programming language',
+        'color' => 'bg-secondary'
+    ],
+    [
+        'name' => 'C++',
+        'description' => 'The C++ programming language',
+        'color' => 'bg-danger'
+    ],
+    [
+        'name' => 'Shell',
+        'description' => 'The Shell programming language',
+        'color' => 'bg-success'
+    ],
+    [
+        'name' => 'Python',
+        'description' => 'The Python programming language',
+        'color' => 'bg-primary-subtle text-dark'
+    ],
+    [
+        'name' => 'JavaScript',
+        'description' => 'The JavaScript programming language',
+        'color' => 'bg-warning text-dark'
+    ],
+    [
+        'name' => 'HTML',
+        'description' => 'The HTML programming language',
+        'color' => 'bg-danger-subtle text-dark'
+    ],
+    [
+        'name' => 'TypeScript',
+        'description' => 'The TypeScript programming language',
+        'color' => 'bg-info-subtle text-dark'
+    ],
+    [
+        'name' => 'GO',
+        'description' => 'The GO programming language',
+        'color' => 'bg-info'
+    ]
+];
